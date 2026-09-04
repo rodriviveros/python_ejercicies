0238ea1e728c70edaef62593ad68a0999efbd04d
Rodrigo Viveros <rodriviveros100@gmail.com>
2026-09-04 11:51:18 -0300

    Primer Ejercicio de Python

---
+++ peliculas/sugerencias.py
@@ -0,0 +1,15 @@
+print("===================================\n")
+print("  App de Sugerencias de Peliculas  \n")
+print("===================================\n")
+usuario =input("Buen día ¿Cúal es tu nombre? ")
+print("¿Que quieres ver hoy, " + usuario + "?\n")
+
+nombre_pelicula="Sherk"
+genero_pelicula="Comedia"
+estreno_pelicula=2001
+rating_pelicula=8.5
+nombre_pelicula2="El Rey Leon"
+genero_pelicula2="Animación"
+estreno_pelicula2=1994
+rating_pelicula2=9.0
+print("Te recomienda la pelicula " + nombre_pelicula2 + " del genero " + genero_pelicula2 + ", estrenada en el año " + str(estreno_pelicula2) + " con un rating de " + str (rating_pelicula2) + " de 10" )
