e3c500f22c548ab0a190b793c790b5bc21fd2c00
Rodrigo Viveros <rodriviveros100@gmail.com>
2026-09-07 11:37:20 -0300

    CATALOGO EXTENDIDO DE PELICULAS Y CONSULTAS POR GENERO
    ·UTILIZAMOS EL IF PARA QUE NOS MUESTRE LAS PELICULAS SEGUN SU GENERO
    ·APRENDIMOS A UTILIZAR LA INDENTACION Y SU IMPORTANCIA PARA LOS BLOQUES DE CÓDIGO

--- peliculas/sugerencias.py
+++ peliculas/sugerencias.py
@@ -1,15 +1,41 @@
-print("===================================\n")
-print("  App de Sugerencias de Peliculas  \n")
-print("===================================\n")
-usuario =input("Buen día ¿Cúal es tu nombre? ")
-print("¿Que quieres ver hoy, " + usuario + "?\n")
 
-nombre_pelicula="Sherk"
-genero_pelicula="Comedia"
-estreno_pelicula=2001
-rating_pelicula=8.5
-nombre_pelicula2="El Rey Leon"
-genero_pelicula2="Animación"
-estreno_pelicula2=1994
-rating_pelicula2=9.0
-print("Te recomienda la pelicula " + nombre_pelicula2 + " del genero " + genero_pelicula2 + ", estrenada en el año " + str(estreno_pelicula2) + " con un rating de " + str (rating_pelicula2) + " de 10" )
+print("=====================================\n")
+print("      sugerencias de pelìculas       \n")
+print("=====================================\n")
+usuario=input("Buen dìa, cùal es tu nombre? ")
+print("¿Què querès ver hoy, "+usuario+"?")
+nombre_pelicula="Rapidos y furiosos"
+genero_pelicula="Acciòn"
+anio_pelicula=2001
+rating_pelicula=6.8
+nombre_pelicula2="Troya"
+genero_pelicula2="Acciòn"
+anio_pelicula2=2004
+rating_pelicula2=7.3
+nombre_pelicula3="Terminator 2"
+genero_pelicula3="Acciòn"
+anio_pelicula3=1991
+rating_pelicula3=8.6
+nombre_pelicula4="¿Y dónde está el piloto?"   
+genero_pelicula4="Comedia"  
+anio_pelicula4=1980 
+rating_pelicula4=7.7 
+nombre_pelicula5="¿Què pasò ayer?"
+genero_pelicula5="Comedia"
+anio_pelicula5=2009
+rating_pelicula5=7.7
+print("---- GÉNEROS-----")
+print("Acción")
+print("Comedia")
+genero_favorito=input("¿Que genero te gusta? ")
+print ("Buscando Péliculas del Género: " +genero_favorito)
+if (genero_pelicula==genero_favorito):
+	print(nombre_pelicula)
+if (genero_pelicula2==genero_favorito):
+  print(nombre_pelicula2)
+if (genero_pelicula3==genero_favorito):
+  print(nombre_pelicula3)
+if (genero_pelicula4==genero_favorito):
+	print(nombre_pelicula4)
+if (genero_pelicula5==genero_favorito):
+	print(nombre_pelicula5)
